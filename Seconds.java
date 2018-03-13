aa4c713b386db296e7a72518e426d8323d0f6019
A_Oikawa <asmaaayyash22@gmail.com>
2018-03-13 17:33:49 +0300

    improving

--- Seconds.java
+++ Seconds.java
@@ -11,14 +11,13 @@ public class Asignement {
 
   
     public static void main(String[] args) {
-       int seconds = 5000;
+     Scanner s = new Scanner(System.in);
+       int seconds = s.nextInt();
        int miniutesInSeconds = seconds /60;
        int hoursInSeconds = seconds / 3600;
        int secondsInSeconds = seconds%60;
        int minutes = miniutesInSeconds%60;
-       System.out.println(secondsInSeconds);
-       System.out.println(hoursInSeconds);
-       System.out.println(minutes);
+       System.out.println("number of hours = "+hoursInSeconds + ", number of min =  "+ minutes + " seconds = " +secondsInSeconds);
 
    }
     
