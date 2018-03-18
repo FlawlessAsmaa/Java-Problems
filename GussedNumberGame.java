fb95c739f6e976be48126ac6175e0a07a0a2b73f
A_Oikawa <asmaaayyash22@gmail.com>
2018-03-18 03:25:35 +0300

    Update GussedNumberGame.java

--- GussedNumberGame.java
+++ GussedNumberGame.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gussednubergame;
+package gussednumbergame;
 
 import java.util.Random;
 import java.util.Scanner;
 
 
-public class GussedNuberGame {
+public class GussedNumberGame {
 
     
     public static void main(String[] args) {
