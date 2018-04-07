4cda05b7fdd5b863eac42db2f6243605c15678a5
A_Oikawa <asmaaayyash22@gmail.com>
2018-04-07 18:11:44 +0300

    Date format American And European

---
+++ AmericanFormat.java
@@ -0,0 +1,24 @@
+
+package americanformat;
+
+
+public class AmericanFormat {
+
+
+    public static void main(String[] args) {
+        
+          printAmerican("Sat", "April", 7, 2018);
+          printEuropean("Fri", "Dec", 21, 1991);
+          
+    }
+    
+    
+     public static void printAmerican (String day, String month, int date, int year) {
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        
+    }
+     
+     public static void printEuropean(String day, String month, int date, int year) {
+         System.out.println(day + ' ' + date + ' ' + month + ", " + year);
+     }
+}
